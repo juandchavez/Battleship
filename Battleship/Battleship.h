59cdc38cfe6b28b89332036f4e7a32b2cae3aab7
Juan Chavez <juandchavez00@gmail.com>
2024-06-04 00:22:56 -0700

    Created setup Need to create gameboard again

--- Battleship/Battleship.h
+++ Battleship/Battleship.h
@@ -1,8 +1,19 @@
 ﻿// Battleship.h : Include file for standard system include files,
 // or project specific include files.
 
-#pragma once
-
 #include <iostream>
+#pragma once
+#ifndef BATTLESHIP_H
+#define BATTLESHIP_H
+
+class Battleship {
+private:
+
+public:
+
+};
+
+#endif 
+
 
 // TODO: Reference additional headers your program requires here.
